20cfecc9c1c1e35afb492b66722ed157328f77a3
Harika0308 <121936507+Harika0308@users.noreply.github.com>
2026-03-10 10:26:01 +0530

    Add files via upload

---
+++ Solution1.java
@@ -0,0 +1,22 @@
+class Solution1 {
+    public int frogJump(int[] height) {
+
+        int n = height.length;
+        int[] dp = new int[n];
+
+        dp[0] = 0;
+
+        for(int i = 1; i < n; i++){
+
+            int jump1 = dp[i-1] + Math.abs(height[i] - height[i-1]);
+
+            int jump2 = Integer.MAX_VALUE;
+            if(i > 1)
+                jump2 = dp[i-2] + Math.abs(height[i] - height[i-2]);
+
+            dp[i] = Math.min(jump1, jump2);
+        }
+
+        return dp[n-1];
+    }
+}
